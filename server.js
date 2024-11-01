61807d4514c368d7df2729074daf302fe129f843
DavidXploidz <david.adame.itd@gmail.com>
2024-11-01 15:01:48 -0600

    secure true

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.post('/send-email', (req, res) => {
     const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
         port: 465,
-        secure: false,
+        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
